751360b23e79c6df218fffdb40288d20905284ca
Zhaniya <shampun.zhumaisynba01@gmail.com>
2023-02-03 20:45:25 +0600

    123

--- inf/d.py
+++
@@ -1,5 +0,0 @@
-a = list(map(int, input().split()))
-
-for i in range(0, len(a) - 1):
-    if a[i] > a[i-1]:
-        print(a[i], end=" ")
--- inf/e.py
+++
@@ -1,6 +0,0 @@
-a = list(map(int, input().split()))
-
-for i in range(0, len(a) - 1):
-    if a[i] * a[i+1] > 0:
-        print(a[i], a[i+1], end=" ")
-        exit()
